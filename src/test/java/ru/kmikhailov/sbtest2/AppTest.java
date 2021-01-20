6a9c575da0e4ebbf503e2e5b6c05248b4d566e5a
Kirill Mikhailov <kmikhailov@mail.ru>
2021-01-21 00:45:52 +0500

    let's start

---
+++ src/test/java/ru/kmikhailov/sbtest2/AppTest.java
@@ -0,0 +1,17 @@
+package ru.kmikhailov.sbtest2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class AppTest {
+
+    @Test
+    public void testDummy() {
+    }
+}
